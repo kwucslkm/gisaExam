c7fad00abc774221a3dd52b3a4146abaf2d473c9
kwucslkm <kwucsani@naver.com>
2023-07-07 17:07:16 +0900

    gisafinished2

---
+++ JAVA/exam_.java
@@ -0,0 +1,18 @@
+public class exam_ {
+    public static void main(String[] args) {
+        int num = 5;
+        int t = 3;
+        int out;
+        int outPrint = 0;
+        out = (num>t) ? 10 : 20;
+        for (int i=0 ; i < out ; i++){
+            t += i;
+            if(t> out){
+                outPrint +=i;
+
+            }
+            i++;
+        }
+        System.out.println(outPrint);
+    }
+}
