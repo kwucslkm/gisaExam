f9035e6186a55b5ce8daf3b4eace6dab31879af1
kwucslkm <kwucsani@naver.com>
2023-07-12 18:07:09 +0900

    gisa230712 after commit

---
+++ PythonExam/ch10_while.py
@@ -0,0 +1,6 @@
+a=["안녕", "헬로", "니하오", "콘니치와"]
+aa= (input("인사말 입력> "))
+while aa in a :
+    print(aa)
+    aa =  (input("인사말 입력> "))
+print("없는 인사말 입니다.")
---
+++ PythonExam/ch12_for.py
@@ -0,0 +1,4 @@
+i = 0
+for i in range(1,6):
+    num = int(input("숫자입력> "))
+    if 
